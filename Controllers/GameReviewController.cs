d7238dfbc32068d76cd1fa2b473127ad6e92e6c1
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-02 14:11:05 +1100

    create GET, POST api

--- Controllers/GameReviewController.cs
+++ Controllers/GameReviewController.cs
@@ -1,3 +1,4 @@
+using Game_review_project.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,9 +8,37 @@ namespace Game_review_project.Controllers
     [ApiController]
     public class NewsController : ControllerBase 
     {
+        private static List<GameNewsModel> _news = new List<GameNewsModel>{
+            new GameNewsModel { Id = 1, Title = "Hollow Knight", Description = "hello...", Category = Category.Indie},
+            new GameNewsModel { Id = 1, Title = "Zelda: Breath of the Wild", Description = "hello...", Category = Category.Tripple_A},
+            new GameNewsModel { Id = 1, Title = "Than Trung", Description = "hello...", Category = Category.Indie}
+        };
+
+        // GET: api/news
         [HttpGet]
-        public string Get() {
-            return "Returning";
+        public ActionResult<IEnumerable<GameNewsModel>> GetAllNews() {
+            return _news;
+        }
+
+        // GET: api/news/1
+        [HttpGet("{id}")]
+        public ActionResult<GameNewsModel> GetNews(int id)
+        {
+            var news = _news.FirstOrDefault(item => item.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return news;
+        }
+
+        // POST: api/news
+        [HttpPost]
+        public ActionResult<GameNewsModel> PostNews(GameNewsModel news)
+        {
+            _news.Add(news);
+            return CreatedAtAction(nameof(GetNews), new {id = news.Id}, news);
         }
     }
 }
--- Models/GameNewsModel.cs
+++ Models/GameNewsModel.cs
@@ -5,6 +5,10 @@ namespace Game_review_project.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Category { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public enum Category {
+        Tripple_A, Indie, Live_Services
     }
 }
