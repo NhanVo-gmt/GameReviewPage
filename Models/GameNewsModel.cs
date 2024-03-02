bb40094a1fec2e482a502bec36cb4e9f75df1a3f
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-02 13:54:06 +1100

    adding game news model

--- Models/GameNewsModel.cs
+++ Models/GameNewsModel.cs
@@ -1,6 +1,10 @@
 namespace Game_review_project.Models
 {
-    public class GameNewsModel {
-
+    public class GameNewsModel 
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
     }
 }
