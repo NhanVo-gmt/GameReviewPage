33a55802372d1053e1cd3ba57e79274364e40b75
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-04 21:35:11 +1100

    adding order model

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -8,21 +8,21 @@ namespace OrderManagementSystem.Controllers
     [ApiController]
     public class NewsController : ControllerBase 
     {
-        private static List<OrderModel> _news = new List<OrderModel>{
-            new OrderModel { Id = 1, Title = "Hollow Knight", Description = "hello..."},
-            new OrderModel { Id = 1, Title = "Zelda: Breath of the Wild", Description = "hello..."},
-            new OrderModel { Id = 1, Title = "Than Trung", Description = "hello..."}
+        private static List<Product> _news = new List<Product>{
+            new Product { Id = 1, Title = "Hollow Knight", Description = "hello..."},
+            new Product { Id = 1, Title = "Zelda: Breath of the Wild", Description = "hello..."},
+            new Product { Id = 1, Title = "Than Trung", Description = "hello..."}
         };
 
         // GET: api/news
         [HttpGet]
-        public ActionResult<IEnumerable<OrderModel>> GetAllNews() {
+        public ActionResult<IEnumerable<Product>> GetAllNews() {
             return _news;
         }
 
         // GET: api/news/1
         [HttpGet("{id}")]
-        public ActionResult<OrderModel> GetNews(int id)
+        public ActionResult<Product> GetNews(int id)
         {
             var news = _news.FirstOrDefault(item => item.Id == id);
             if (news == null)
@@ -35,7 +35,7 @@ namespace OrderManagementSystem.Controllers
 
         // POST: api/news
         [HttpPost]
-        public ActionResult<OrderModel> PostNews(OrderModel news)
+        public ActionResult<Product> PostNews(Product news)
         {
             _news.Add(news);
             return CreatedAtAction(nameof(GetNews), new {id = news.Id}, news);
--- Models/OrderModel.cs
+++ Models/OrderModel.cs
@@ -1,10 +1,16 @@
 namespace OrderManagementSystem.Models
 {
-    public class OrderModel 
+    public class Order {
+        public int OrderID { get; set; }
+        public DateTime Date { get; set; }
+        public ICollection<OrderDetail> OrderDetails { get; set;} 
+    }
+
+    public class OrderDetail
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
+        public int OrderDetailID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
     }
 }
