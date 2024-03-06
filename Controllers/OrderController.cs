1e84c7a85141904dc14eccaa2655d70ef65b52d0
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-06 15:01:34 +1100

    adding http for testing order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,9 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using OrderManagementSystem.Models;
 
 namespace OrderManagementSystem.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class OrderController : ControllerBase 
     {
+        private static List<OrderDetail> _orderDetails = new List<OrderDetail>{
+            new OrderDetail { OrderDetailID = 1, OrderID = 1, ProductID = 1, Quantity = 2},
+            new OrderDetail { OrderDetailID = 1, OrderID = 2, ProductID = 2, Quantity = 2},
+            new OrderDetail { OrderDetailID = 1, OrderID = 3, ProductID = 3, Quantity = 1},
+        };
 
+        private static List<Order> _orders = new List<Order>{
+            new Order { OrderID = 1, OrderDetails = _orderDetails}
+        }; 
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetAllOrders()
+        {
+            if (_orders == null)
+            {
+                return NotFound();
+            }
+            return Ok(_orders);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrderFromId(int id)
+        {
+            var order = _orders.FirstOrDefault(item => item.OrderID == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(order);
+        }
     }
 }
