2f21a88b354ce488c47bef14423abd588fe9431a
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-16 18:17:30 +1100

    adding fake package

---
+++ Tests/OrderTests.cs
@@ -0,0 +1,17 @@
+using OrderManagementSystem.Services;
+using Xunit;
+
+namespace OrderManagementSystem.Tests 
+{
+    
+    public class OrderControllerTest 
+    {
+        [Fact]
+        public void Test1()
+        {
+        //    var dataStore = A
+        }
+    }
+}
+
+
